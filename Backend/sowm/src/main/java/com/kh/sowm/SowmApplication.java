bee2ba2c958abc94e2200cc72dd1585b3b95d40d
aotlfekdl <tmdals3603@naver.com>
2025-05-28 09:54:26 +0900

    add backend folder

---
+++ Backend/sowm/src/main/java/com/kh/sowm/SowmApplication.java
@@ -0,0 +1,13 @@
+package com.kh.sowm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SowmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SowmApplication.class, args);
+	}
+
+}
---
+++ Backend/sowm/src/test/java/com/kh/sowm/SowmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.sowm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SowmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
