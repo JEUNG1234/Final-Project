9f8e365c63d99433f9b3818c7e97318d44cbfc4c
aotlfekdl <tmdals3603@naver.com>
2025-06-17 09:44:11 +0900

    add 엔티티 추가

---
+++ Backend/sowm/src/main/java/com/kh/sowm/entity/Category.java
@@ -0,0 +1,18 @@
+package com.kh.sowm.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+@Entity
+@Table(name = "CATEGORY")
+public class Category {
+    @Id
+    private Long categoryNo;
+
+
+    private String categoryName;
+}
