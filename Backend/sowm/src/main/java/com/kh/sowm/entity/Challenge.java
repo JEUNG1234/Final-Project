d4d9c29fcdde1fbeceba12d3f147f492d2fd0780
DESKTOP-C5J8I0B\user1 <ayinyun@naver.com>
2025-06-17 20:19:27 +0900

    yml파일 삭제 및 프론트 myworkation 생성

--- Backend/sowm/src/main/java/com/kh/sowm/entity/Challenge.java
+++ Backend/sowm/src/main/java/com/kh/sowm/entity/Challenge.java
@@ -13,12 +13,28 @@ import lombok.*;
 @Table(name = "CHALLENGE")
 public class Challenge {
     @Id
+    @Column(name = "CHALLENGE_NO", length = 30, unique = true, nullable = false)
     private Long challengeNo;
-    private String userId;
-    private Long voteNo;
-    private Long voteContentNo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User userId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "VOTE_NO", nullable = false)
+    private Vote voteNo;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "VOTE_CONTENT_N0")
+    private VoteContent voteContentNo;
+
+    @Column(name = "CHALLENGE_START_DATE")
     private LocalDate challengeStartDate;
+
+    @Column(name = "CHALLENGE_END_DATE")
     private LocalDate challengeEndDate;
+
+    @Column(name = "CHALLENGE_POINT")
     private int challengePoint;
-    private String field;
+
 }
