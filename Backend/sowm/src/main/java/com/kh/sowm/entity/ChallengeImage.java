8cc0d843861b00cf6109d444f51045c1710a0758
aotlfekdl <tmdals3603@naver.com>
2025-06-18 10:12:39 +0900

    add 엔티티 추가

--- Backend/sowm/src/main/java/com/kh/sowm/entity/ChallengeImage.java
+++ Backend/sowm/src/main/java/com/kh/sowm/entity/ChallengeImage.java
@@ -1,5 +1,6 @@
 package com.kh.sowm.entity;
 
+import com.kh.sowm.enums.CommonEnums;
 import jakarta.persistence.*;
 import java.time.LocalDate;
 
@@ -12,13 +13,44 @@ import lombok.*;
 @Entity
 @Table(name = "CHALLENGE_IMAGE")
 public class ChallengeImage {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "FILE_NO")
     private Long fileNo;
-    private Long completeNo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "COMPLETE_NO", nullable = false)
+    private ChallengeComplete challengeComplete;
+
+    @Column(name = "ORIGINAL_NAME", nullable = false, length = 255)
     private String originalName;
+
+    @Column(name = "CHANGED_NAME", nullable = false, length = 255)
     private String changedName;
+
+    @Column(name = "PATH", nullable = false, length = 255)
     private String path;
+
+    @Column(name = "SIZE", nullable = false)
     private Long size;
+
+    @Column(name = "UPLOAD_DATE", nullable = false)
     private LocalDate uploadDate;
-    private String status;
+
+    @Column(length = 1, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CommonEnums.Status status;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.uploadDate == null) {
+            this.uploadDate = LocalDate.now();
+        }
+        if(this.status == null) {
+            this.status = CommonEnums.Status.Y;
+        }
+
+    }
+
 }
