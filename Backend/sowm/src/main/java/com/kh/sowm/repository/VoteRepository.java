fd7201ae3f1f1e76fa1568ad2df8e18ca709c7f1
DESKTOP-LF4A7MV\user1 <jmg9152@hs.ac.kr>
2025-06-20 12:39:44 +0900

    no message

--- Backend/sowm/src/main/java/com/kh/sowm/repository/VoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kh.sowm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//Vote 엔티티에 대한 데이터베이스 작업을 처리하는 JpaRepository
-public interface VoteRepository extends JpaRepository<Vote, Long> {
-}
