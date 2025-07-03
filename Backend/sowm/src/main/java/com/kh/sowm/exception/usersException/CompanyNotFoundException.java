430eeaf84a8871a3bf5bf44783c5747f611df3e0
aotlfekdl <tmdals3603@naver.com>
2025-07-03 11:12:03 +0900

    워케이션 수정

---
+++ Backend/sowm/src/main/java/com/kh/sowm/exception/usersException/CompanyNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kh.sowm.exception.usersException;
+
+public class CompanyNotFoundException extends RuntimeException {
+  public CompanyNotFoundException(String message) {
+    super(message);
+  }
+}
